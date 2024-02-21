0ac30de57a83c1136b0975052c7f59b11ccc2431
celian <celian.lombardot@epitech.eu>
2024-02-21 10:54:54 +0100

    Add usage

--- app/Main.hs
+++ app/Main.hs
@@ -14,9 +14,21 @@ import Lib (defaultArgs, Args(..))
 import ParseArgs (getOpts)
 import DisplayLines (wolfram)
 
+usage :: String
+usage = "USAGE:\n\n" ++
+        "./wolfram --rule value [--start value] [--lines value]" ++
+        " [--window value] [--move value]\n\n" ++
+        "--rule    ->   select the rule to apply\n" ++
+        "--start   ->   the first line of the generation to display\n" ++
+        "--lines   ->   the number of lines to display\n" ++
+        "--window  ->   the size of the display window\n" ++
+        "--move    ->   translation to apply to the window " ++
+        "(right if positive, left if negative)"
+
+
 handleErrors :: Maybe Args -> IO()
 handleErrors Nothing
-    = putStrLn "Invalid arguments" >> exitWith (ExitFailure 84)
+    = putStrLn usage >> exitWith (ExitFailure 84)
 handleErrors (Just arg) = wolfram arg
 
 main :: IO ()
